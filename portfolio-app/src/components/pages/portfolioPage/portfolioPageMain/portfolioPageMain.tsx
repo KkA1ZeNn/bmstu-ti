0c4aa76de87496690ef6b68689e557f93a375913
manuilov <manuilov@bolid.ru>
2025-04-18 18:25:45 +0300

    Добавил секцию "о себе"

--- portfolio-app/src/components/pages/portfolioPage/portfolioPageMain/portfolioPageMain.tsx
+++ portfolio-app/src/components/pages/portfolioPage/portfolioPageMain/portfolioPageMain.tsx
@@ -6,13 +6,32 @@ const Main = () => {
         <>
             <article className="myBio">
                 <div className="myBio__info">
-                    <p className="info__name">Даниил Мануйлов</p>
-                    <p className="info__work-position">Frontend developer</p>
+                    <h1 className="info__name">Даниил Мануйлов</h1>
+                    <h2 className="info__work-position">Frontend developer</h2>
                 </div>
                 <div className="myBio__photo">
                     <img src={myPhoto} alt="my photo" />
                 </div>
             </article>
+
+            <section id='aboutMe'>
+                <h3 className='aboutMe__header'>Пара слов обо мне</h3>
+                <div className='aboutMe__text'>
+                    <p>Привет ✌</p>
+                    <p>
+                        В данный момент я учусь в магистратуре МФ МГТУ им. Н. Э. Баумана
+                        по направлению «Информатика и вычисли­тельная техника».
+                    </p>     
+                    <p>
+                        В прошлом году защитил диплом бакалавра на тему «Разработка web-сайта
+                        для работы с интерактивными картами зданий». Работа была высоко оценена комиссией.
+                    </p>
+                    <p>
+                        С декабря 2024 работаю frontend разработчиком в компании "НВП Болид". Планирую получить здесь
+                        необходимую квалификацию и опыт для дальнейшего продвижения по карьерной лестнице.
+                    </p>
+                </div>
+            </section>
         </>
     );
 };
