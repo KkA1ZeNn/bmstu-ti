8a1295c7e3e216a1ec16e6a578d18366869be365
manuilov <manuilov@bolid.ru>
2025-04-18 14:50:33 +0300

    Подготовил реакт приложение с использованием Vite к дальнейшему соданию страницы

---
+++ portfolio-app/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './Header'
+
+function App() {
+
+  return (
+      <div>
+        <Header />
+        <p>Давай создавать крутые приложения!</p>
+      </div>
+  );
+}
+
+export default App
