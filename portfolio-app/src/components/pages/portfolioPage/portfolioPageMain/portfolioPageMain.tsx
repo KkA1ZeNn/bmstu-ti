1a04b767c4495efa70038ca1bc41b5c08d40ef71
SpEcTrEeLeEe <daniilmanuylov239@gmail.com>
2025-05-09 20:51:49 +0300

    contact block

--- portfolio-app/src/components/pages/portfolioPage/portfolioPageMain/portfolioPageMain.tsx
+++ portfolio-app/src/components/pages/portfolioPage/portfolioPageMain/portfolioPageMain.tsx
@@ -3,25 +3,30 @@ import MyBio from './mainMyBioSection/myBioComponent';
 import AboutMe from './mainAboutMeSection/aboutMeComponent';
 import MyInterests from './mainMyInterestsSection/myInterestsComponent';
 import MySkills from './mainMySkillsSection/mySkillsComponent';
+import Contacts from './mainContactsSection/contactsComponent';
 
 const Main = () => {
     return (
         <>
-        <article id="myBio">
-            <MyBio/>
-        </article>
+            <article id="myBio">
+               <MyBio/>
+            </article>
 
-        <section id='aboutMe'>
-            <AboutMe/>
-        </section>
+            <section id='aboutMe'>
+               <AboutMe/>
+            </section>
 
-        <section id='skills'>
-            <MySkills/>
-        </section>
+            <section id='skills'>
+               <MySkills/>
+            </section>
 
-        <section id='interests'>
-            <MyInterests/>
-        </section>
+            <section id='interests'>
+               <MyInterests/>
+            </section>
+
+            <section id='contacts'>
+               <Contacts/>
+            </section>
         </>
     );
 };
